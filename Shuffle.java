2649d896f1998393029fb99c6a0d011cfb23f673
Abdelrhman Ali <abdoali28299@gmail.com>
2018-11-29 22:05:05 +0200

    shuffle return a copy of array

--- Shuffle.java
+++ Shuffle.java
@@ -3,15 +3,20 @@ package com.company;
 import java.util.Random;
 
 public class Shuffle {
-    public void shuffleArray(char[] a) {
-        int n = a.length;
+    public char[] shuffleArray(char[] a) {
+        char [] out = new char [a.length];
+        for (int i=0; i<out.length;i++){
+            out[i] = a[i];
+        }
+        int n = out.length;
         Random random = new Random();
         random.nextInt();
         for (int i = 0; i < n; i++) {
             int rand = random.nextInt(n);
-            char tmp = a[i];
-            a[i] = a[rand];
-            a[rand] = tmp;
+            char tmp = out[i];
+            out[i] = out[rand];
+            out[rand] = tmp;
         }
+        return out;
     }
 }
