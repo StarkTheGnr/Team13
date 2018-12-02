3946591b4f0cf57dbb693ad6d0497620f29bfc17
StarkTheGnr <lordsomy@hotmail.com>
2018-12-02 03:28:59 +0200

    fixing UI

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,21 @@ public class Main
 
 	private JFrame frame;
 	private JTextField txtInput;
-	private JLabel lblNa;
+	private JLabel lblSort;
+	private JLabel lblP;
+	private JLabel lblPT;
+	private JLabel lblROP;
+	private JLabel lblGM;
+	private JLabel lblCP;
+	private JLabel lblDA;
+	private JLabel lblMTN;
+	private JLabel lblMRV;
+	private JLabel lblSA;
+	private JLabel lblMinTN;
+	private JLabel lblSP;
+	private JLabel lblLP;
+	private JLabel lblS;
+	private JLabel label;
 
 	/**
 	 * Launch the application.
@@ -65,16 +79,16 @@ public class Main
 		lblInput.setBounds(10, 11, 46, 14);
 		frame.getContentPane().add(lblInput);
 
-		lblNa = new JLabel("N/A");
-		lblNa.setBounds(66, 305, 332, 14);
-		frame.getContentPane().add(lblNa);
+		lblSort = new JLabel("N/A");
+		lblSort.setBounds(10, 131, 122, 14);
+		frame.getContentPane().add(lblSort);
 
 		JButton btnNewButton = new JButton("Check Sorted");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0)
 			{
 				boolean res = Functions.checksorted(Functions.transformToInt(txtInput.getText()));
-				lblNa.setText(String.valueOf(res));
+				lblSort.setText(String.valueOf(res));
 			}
 		});
 		btnNewButton.setBounds(10, 97, 122, 23);
@@ -85,7 +99,7 @@ public class Main
 			public void actionPerformed(ActionEvent e)
 			{
 				int res = Functions.countPrimes(Functions.transformToInt(txtInput.getText()));
-				lblNa.setText(String.valueOf(res));
+				lblCP.setText(String.valueOf(res));
 			}
 		});
 		btnCountPrimes.setBounds(142, 97, 122, 23);
@@ -96,7 +110,7 @@ public class Main
 			public void actionPerformed(ActionEvent e)
 			{
 				ArrayList<String> res = Functions.DistinctArray(Functions.transformToString(txtInput.getText()));
-				lblNa.setText(String.valueOf(res));
+				lblDA.setText(String.valueOf(res));
 			}
 		});
 		btnDistinctArray.setBounds(276, 97, 122, 23);
@@ -107,14 +121,14 @@ public class Main
 			public void actionPerformed(ActionEvent arg0)
 			{
 				double res = Functions.Get_median(Functions.transformToString(txtInput.getText()));
-				lblNa.setText(String.valueOf(res));
+				lblGM.setText(String.valueOf(res));
 			}
 		});
-		btnGetMedian.setBounds(10, 131, 122, 23);
+		btnGetMedian.setBounds(10, 165, 122, 23);
 		frame.getContentPane().add(btnGetMedian);
 
-		JButton btnMaxThreeNumber = new JButton("Max Three Numbers");
-		btnMaxThreeNumber.addActionListener(new ActionListener() {
+		JButton btnMTN = new JButton("Max Three Numbers");
+		btnMTN.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
 				int[] res = Functions.Max_three_number(Functions.transformToInt(txtInput.getText()));
@@ -123,23 +137,23 @@ public class Main
 				{
 					result += res[i] + ",";
 				}
-				lblNa.setText(result);
+				lblMTN.setText(result);
 			}
 		});
-		btnMaxThreeNumber.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btnMaxThreeNumber.setBounds(142, 131, 122, 23);
-		frame.getContentPane().add(btnMaxThreeNumber);
+		btnMTN.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		btnMTN.setBounds(142, 165, 122, 23);
+		frame.getContentPane().add(btnMTN);
 
 		JButton btnMostRepeatedValue = new JButton("Most Repeated Value");
 		btnMostRepeatedValue.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
 				int res = Functions.most_repeated_value(Functions.transformToInt(txtInput.getText()));
-				lblNa.setText(String.valueOf(res));
+				lblMRV.setText(String.valueOf(res));
 			}
 		});
 		btnMostRepeatedValue.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btnMostRepeatedValue.setBounds(276, 131, 122, 23);
+		btnMostRepeatedValue.setBounds(276, 165, 122, 23);
 		frame.getContentPane().add(btnMostRepeatedValue);
 
 		JButton btnRop = new JButton("ROP");
@@ -152,10 +166,10 @@ public class Main
 				{
 					result += res[i] + ",";
 				}
-				lblNa.setText(result);
+				lblROP.setText(result);
 			}
 		});
-		btnRop.setBounds(10, 165, 122, 23);
+		btnRop.setBounds(10, 233, 122, 23);
 		frame.getContentPane().add(btnRop);
 
 		JButton btnShuffleArray = new JButton("Shuffle Array");
@@ -163,10 +177,10 @@ public class Main
 			public void actionPerformed(ActionEvent e) 
 			{
 				char[] res = Functions.shuffleArray(txtInput.getText().toCharArray());
-				lblNa.setText(new String(res));
+				lblSA.setText(new String(res));
 			}
 		});
-		btnShuffleArray.setBounds(142, 165, 122, 23);
+		btnShuffleArray.setBounds(142, 233, 122, 23);
 		frame.getContentPane().add(btnShuffleArray);
 
 		JButton btnMinThreeNumbers = new JButton("Min Three Numbers");
@@ -179,11 +193,11 @@ public class Main
 				{
 					result += res[i] + ",";
 				}
-				lblNa.setText(result);
+				lblMinTN.setText(result);
 			}
 		});
 		btnMinThreeNumbers.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btnMinThreeNumbers.setBounds(276, 165, 122, 23);
+		btnMinThreeNumbers.setBounds(276, 233, 122, 23);
 		frame.getContentPane().add(btnMinThreeNumbers);
 
 		JButton btnPalindromeTest = new JButton("Palindrome Test");
@@ -191,10 +205,10 @@ public class Main
 			public void actionPerformed(ActionEvent e) 
 			{
 				boolean res = Functions.IsPalindrome(txtInput.getText().toCharArray());
-				lblNa.setText(String.valueOf(res));
+				lblPT.setText(String.valueOf(res));
 			}
 		});
-		btnPalindromeTest.setBounds(10, 199, 122, 23);
+		btnPalindromeTest.setBounds(10, 292, 122, 23);
 		frame.getContentPane().add(btnPalindromeTest);
 
 		JButton btnSmallestPrime = new JButton("Smallest Prime");
@@ -203,10 +217,10 @@ public class Main
 			{
 				int[] arr = Functions.transformToInt(txtInput.getText());
 				int res = Functions.SmallestPrime(arr, arr.length);
-				lblNa.setText(String.valueOf(res));
+				lblSP.setText(String.valueOf(res));
 			}
 		});
-		btnSmallestPrime.setBounds(142, 199, 122, 23);
+		btnSmallestPrime.setBounds(142, 292, 122, 23);
 		frame.getContentPane().add(btnSmallestPrime);
 
 		JButton btnLargestPrime = new JButton("Largest Prime");
@@ -215,10 +229,10 @@ public class Main
 			{
 				int[] res = Functions.ROP(Functions.transformToInt(txtInput.getText()));
 				int result = Functions.getLargest(res);
-				lblNa.setText(String.valueOf(result));
+				lblLP.setText(String.valueOf(result));
 			}
 		});
-		btnLargestPrime.setBounds(276, 199, 122, 23);
+		btnLargestPrime.setBounds(276, 292, 122, 23);
 		frame.getContentPane().add(btnLargestPrime);
 
 		JButton btnPositive = new JButton("Positive");
@@ -226,10 +240,10 @@ public class Main
 			public void actionPerformed(ActionEvent e) 
 			{
 				ArrayList<String> res = Functions.positiveArray(new ArrayList<String>(Arrays.asList(Functions.transformToString(txtInput.getText()))));
-				lblNa.setText(String.valueOf(res));
+				lblP.setText(String.valueOf(res));
 			}
 		});
-		btnPositive.setBounds(10, 233, 122, 23);
+		btnPositive.setBounds(10, 356, 122, 23);
 		frame.getContentPane().add(btnPositive);
 
 		JButton btnShift = new JButton("Shift");
@@ -237,18 +251,70 @@ public class Main
 			public void actionPerformed(ActionEvent e) 
 			{
 				char[] res = Functions.shift(txtInput.getText().toCharArray());
-				lblNa.setText(new String(res));
+				lblS.setText(new String(res));
 			}
 		});
-		btnShift.setBounds(142, 233, 122, 23);
+		btnShift.setBounds(142, 356, 122, 23);
 		frame.getContentPane().add(btnShift);
 
 		JButton btnRunAll = new JButton("Run All");
-		btnRunAll.setBounds(276, 233, 122, 23);
+		btnRunAll.setBounds(276, 356, 122, 23);
 		frame.getContentPane().add(btnRunAll);
 
-		JLabel lblOutput = new JLabel("Output:");
-		lblOutput.setBounds(10, 305, 46, 14);
-		frame.getContentPane().add(lblOutput);
+		lblPT = new JLabel("N/A");
+		lblPT.setBounds(10, 331, 122, 14);
+		frame.getContentPane().add(lblPT);
+		
+		lblGM = new JLabel("N/A");
+		lblGM.setBounds(10, 199, 122, 14);
+		frame.getContentPane().add(lblGM);
+		
+		lblROP = new JLabel("N/A");
+		lblROP.setBounds(10, 267, 122, 14);
+		frame.getContentPane().add(lblROP);
+		
+		lblCP = new JLabel("N/A");
+		lblCP.setBounds(142, 131, 122, 14);
+		frame.getContentPane().add(lblCP);
+		
+		lblMTN = new JLabel("N/A");
+		lblMTN.setBounds(142, 199, 122, 14);
+		frame.getContentPane().add(lblMTN);
+		
+		lblSA = new JLabel("N/A");
+		lblSA.setBounds(142, 267, 122, 14);
+		frame.getContentPane().add(lblSA);
+		
+		lblSP = new JLabel("N/A");
+		lblSP.setBounds(142, 326, 122, 14);
+		frame.getContentPane().add(lblSP);
+		
+		lblDA = new JLabel("N/A");
+		lblDA.setBounds(276, 131, 122, 14);
+		frame.getContentPane().add(lblDA);
+		
+		lblMRV = new JLabel("N/A");
+		lblMRV.setBounds(276, 199, 122, 14);
+		frame.getContentPane().add(lblMRV);
+		
+		lblMinTN = new JLabel("N/A");
+		lblMinTN.setBounds(276, 267, 122, 14);
+		frame.getContentPane().add(lblMinTN);
+		
+		lblLP = new JLabel("N/A");
+		lblLP.setBounds(276, 326, 122, 14);
+		frame.getContentPane().add(lblLP);
+		
+		lblP = new JLabel("N/A");
+		lblP.setBounds(10, 390, 122, 14);
+		frame.getContentPane().add(lblP);
+		
+		lblS = new JLabel("N/A");
+		lblS.setBounds(142, 390, 122, 14);
+		frame.getContentPane().add(lblS);
+		
+		label = new JLabel("N/A");
+		label.setBounds(276, 390, 122, 14);
+		frame.getContentPane().add(label);
 	}
 }
